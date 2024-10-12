7e9dc7b98eaa00c628d30bc9f09ed023afb0aa76
EaeDave <EaeDav3@gmail.com>
2024-10-12 12:50:14 -0300

    opcoes converted from list to OPCOES tuple + choose validation implementation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+from time import sleep
+from os import system
 
 def exibir_escolha():
     print(f'Escolha do Player: {escolha_player.capitalize()}.')
@@ -9,7 +11,7 @@ def exibir_pontuacao():
     print(f'Pontuação Player = {pontuacao_player}\nPontuação Máquina = {pontuacao_maquina}')
     print('-' * 50)
 
-opcoes = ['pedra', 'papel', 'tesoura']
+OPCOES = ('pedra', 'papel', 'tesoura')
 escolha_valida = False
 pontuacao_player = 0
 pontuacao_maquina = 0
@@ -17,54 +19,58 @@ finalizar_partida = False
 PONTUACAO_MAXIMA = 5
 
 while finalizar_partida == False:
-    escolha_maquina = random.choices(opcoes)[0]
-    escolha_player = input('Escolha entre, "Pedra", "Papel"e "Tesoura": ').lower()
+    escolha_maquina = random.choices(OPCOES)[0]
+    escolha_player = input('Escolha entre, "Pedra", "Papel" e "Tesoura": ').lower()
+    
     while escolha_valida == False:
-        if escolha_player not in opcoes:
+        if escolha_player not in OPCOES:
             print('Escolha inválida.')
+            sleep(1)
+            break
         else:
             escolha_valida = True
+            system('cls')
+            exibir_escolha()
+            
+            
+            # Possiblidades de vitória
+            if escolha_player == 'pedra' and escolha_maquina == 'tesoura':
+                print('O Player venceu a partida!')
+                pontuacao_player +=1
+            elif escolha_player == 'papel' and escolha_maquina == 'pedra':
+                print('O Player venceu a partida!')
+                pontuacao_player +=1
+            elif escolha_player == 'tesoura' and escolha_maquina == 'papel':
+                print('O Player venceu a partida!')
+                pontuacao_player +=1
+                
+                
+            # Possibilidades de derrota
+            if escolha_maquina == 'pedra' and escolha_player == 'tesoura':
+                print('A máquina venceu a partida!')
+                pontuacao_maquina += 1
+            elif escolha_maquina == 'papel' and escolha_player == 'pedra':
+                print('A máquina venceu a partida!')
+                pontuacao_maquina += 1
+            elif escolha_maquina == 'tesoura' and escolha_player == 'papel':
+                print('A máquina venceu a partida!')
+                pontuacao_maquina += 1
+                
+                
+            # Empate
+            if escolha_player == escolha_maquina:
+                print('Empatou!')
+                
+            
+            if pontuacao_player == PONTUACAO_MAXIMA:
+                exibir_pontuacao()
+                print('Pontuação máxima atingida, o Player venceu!')
+                break
+            elif pontuacao_maquina == PONTUACAO_MAXIMA:
+                exibir_pontuacao()
+                print('Pontuação máxima atingida, a Máquina venceu!')
+                break
 
-    exibir_escolha()
-
-    # Possiblidades de vitória
-    if escolha_player == 'pedra' and escolha_maquina == 'tesoura':
-        print('O Player venceu a partida!')
-        pontuacao_player +=1
-    elif escolha_player == 'papel' and escolha_maquina == 'pedra':
-        print('O Player venceu a partida!')
-        pontuacao_player +=1
-    elif escolha_player == 'tesoura' and escolha_maquina == 'papel':
-        print('O Player venceu a partida!')
-        pontuacao_player +=1
-
-
-    # Possibilidades de derrota
-    if escolha_maquina == 'pedra' and escolha_player == 'tesoura':
-        print('A máquina venceu a partida!')
-        pontuacao_maquina += 1
-    elif escolha_maquina == 'papel' and escolha_player == 'pedra':
-        print('A máquina venceu a partida!')
-        pontuacao_maquina += 1
-    elif escolha_maquina == 'tesoura' and escolha_player == 'papel':
-        print('A máquina venceu a partida!')
-        pontuacao_maquina += 1
-
-
-    # Empate
-    if escolha_player == escolha_maquina:
-        print('Empatou!')
-    
-
-    if pontuacao_player == PONTUACAO_MAXIMA:
-        exibir_pontuacao()
-        print('Pontuação máxima atingida, o Player venceu!')
-        break
-    elif pontuacao_maquina == PONTUACAO_MAXIMA:
-        exibir_pontuacao()
-        print('Pontuação máxima atingida, a Máquina venceu!')
-        break
-
-
-    exibir_pontuacao()
+            exibir_pontuacao()
+    escolha_valida = False
 
